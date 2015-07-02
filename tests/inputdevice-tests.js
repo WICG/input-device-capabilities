81f4dbc1beaff5ce1c4dd6a90860ffbab0e9b067
Rick Byers <rbyers@chromium.org>
2015-07-02 16:23:51 -0400

    Add keyboard touch and wheel tests

--- tests/inputdevice-tests.js
+++ tests/inputdevice-tests.js
@@ -28,4 +28,63 @@
   } else {
     window.addEventListener("load", runTestsAndDone);
   }
+  
+  var events;
+  var eventsReceived = {};
+  var sourceDeviceReceived;
+  var target;
+  var asyncTests = [];
+
+  function setEventList() {
+    var expectedEvents = '';
+    for (var i = 0; i < events.length; i++) {
+      if (!(events[i] in eventsReceived)) {
+        expectedEvents += events[i] + ' ';
+      }
+    }
+    document.getElementById("eventList").textContent = expectedEvents;
+  }
+
+  function checkForCompletion() {
+    if (Object.getOwnPropertyNames(eventsReceived).length == events.length) {
+      for(var i = 0; i < asyncTests.length; i++) {
+        asyncTests[i].done();
+      }
+    }  
+  }
+
+  function generateAsyncEventTest(eventName, inputDeviceValidator) {
+    var asyncTest = async_test(eventName + " event has sourceDevice set correctly set");
+    target.addEventListener(eventName, function(event) {
+      asyncTest.step(function() {
+        assert_true(event instanceof UIEvent, "event is a UIEvent");
+        if (sourceDeviceReceived) {
+            assert_equals(event.sourceDevice, sourceDeviceReceived, "sourceDevice is the same object as previous events");
+        } else {
+          inputDeviceValidator(event.sourceDevice);
+          sourceDeviceReceived = event.sourceDevice;
+        }
+      });
+      // We want to monitor for multiple occurneces of an event until all events have been
+      // received.  If some events are never received, those tests will appear as "not run"
+      // while the others will be "timeout".
+      if (!(eventName in eventsReceived)) {
+        eventsReceived[eventName] = true;
+        setEventList();
+        checkForCompletion();
+      }
+    });
+    return asyncTest;
+  }
+
+  window.runInputDeviceEventTests = function(eventNames, inputDeviceValidator) {
+    events = eventNames;
+    setEventList();
+    target = document.getElementById("target");
+    
+    for (var i = 0; i < eventNames.length; i++) {
+      asyncTests.push(generateAsyncEventTest(eventNames[i], inputDeviceValidator));
+    }
+  };
 })();
+
